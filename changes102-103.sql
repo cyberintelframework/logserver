ecd7e54873278b36ee33226cd924654bd67a81c6
Kees Trippelvitz <github@nabber.nl>
2006-11-21 10:06:12 +0000

    Added missing changes102-103.sql file
    git-svn-id: https://svn.surfnet.nl/svn/ids/logserver/tags/rc1-1.03@78 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

---
+++ changes102-103.sql
@@ -0,0 +1,118 @@
+-- SURFnet IDS SQL changes for 1.03
+-- Version: 1.03.01
+-- 30-10-2006
+
+-- Changelog
+-- Removed the timeunit from table login
+-- 1.03.01 Initial release
+
+CREATE TABLE sessions (
+    id serial NOT NULL,
+    sid character varying NOT NULL,
+    ip inet NOT NULL,
+    ts integer NOT NULL,
+    username character varying
+);
+
+GRANT INSERT,SELECT,UPDATE,DELETE ON TABLE sessions TO idslog;
+
+GRANT SELECT,UPDATE ON TABLE sessions_id_seq TO idslog;
+
+CREATE TABLE rrd (
+    id serial NOT NULL,
+    orgid integer NOT NULL,
+    "type" character varying NOT NULL,
+    label character varying NOT NULL,
+    image character varying NOT NULL,
+    "timestamp" integer
+);
+
+ALTER TABLE ONLY rrd
+    ADD CONSTRAINT primary_rrd PRIMARY KEY (id);
+
+GRANT INSERT,SELECT,UPDATE,DELETE ON TABLE rrd TO idslog;
+
+GRANT SELECT,UPDATE ON TABLE rrd_id_seq TO idslog;
+
+CREATE TABLE report (
+    id serial NOT NULL,
+    user_id integer NOT NULL,
+    enabled boolean NOT NULL,
+    email character varying,
+    gpg_enabled boolean,
+    subject character varying
+);
+
+ALTER TABLE ONLY report
+    ADD CONSTRAINT primary_report PRIMARY KEY (id);
+
+ALTER TABLE ONLY report
+    ADD CONSTRAINT unique_report_user_id UNIQUE (user_id);
+
+ALTER TABLE ONLY report
+    ADD CONSTRAINT foreign_report_user_id FOREIGN KEY (user_id) REFERENCES "login"(id);
+
+GRANT INSERT,SELECT,UPDATE,DELETE ON TABLE report TO idslog;
+
+GRANT SELECT,UPDATE ON TABLE report_id_seq TO idslog;
+
+CREATE TABLE report_content (
+    id serial NOT NULL,
+    report_id integer,
+    title character varying,
+    "template" integer,
+    last_sent integer,
+    active boolean,
+    sensor_id integer,
+    frequency integer,
+    "interval" integer,
+    priority integer
+);
+
+ALTER TABLE ONLY report_content
+    ADD CONSTRAINT primary_report_content PRIMARY KEY (id);
+
+ALTER TABLE ONLY report_content
+    ADD CONSTRAINT foreign_report_content_report_id FOREIGN KEY (report_id) REFERENCES report(id);
+
+GRANT INSERT,SELECT,UPDATE,DELETE ON TABLE report_content TO idslog;
+
+GRANT SELECT,UPDATE ON TABLE report_content_id_seq TO idslog;
+
+CREATE TABLE report_template_threshold (
+    id serial NOT NULL,
+    report_content_id integer NOT NULL,
+    target integer,
+    value integer,
+    deviation integer,
+    "operator" integer
+);
+
+ALTER TABLE ONLY report_template_threshold
+    ADD CONSTRAINT primary_report_template_threshold PRIMARY KEY (id);
+
+ALTER TABLE ONLY report_template_threshold
+    ADD CONSTRAINT foreign_report_template_threshold_report_content_id FOREIGN KEY (report_content_id) REFERENCES report_content(id);
+
+GRANT INSERT,SELECT,UPDATE,DELETE ON TABLE report_template_threshold TO idslog;
+
+GRANT SELECT,UPDATE ON TABLE report_template_threshold_id_seq TO idslog;
+
+
+CREATE TABLE org_id (
+    id serial NOT NULL,
+    orgid integer NOT NULL,
+    identifier character varying NOT NULL
+);
+
+ALTER TABLE ONLY org_id
+    ADD CONSTRAINT primary_org_id_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY org_id
+    ADD CONSTRAINT unique_identifier UNIQUE (identifier);
+
+GRANT INSERT,SELECT,UPDATE,DELETE ON TABLE org_id TO idslog;
+
+GRANT SELECT,UPDATE ON TABLE org_id_id_seq TO idslog;
+
+ALTER TABLE login ADD COLUMN serverhash character varying;
