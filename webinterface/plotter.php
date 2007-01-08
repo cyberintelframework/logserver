9311db67414cf8a7da9c9efe97a49f3528b9197a
Kees Trippelvitz <github@nabber.nl>
2007-01-08 16:09:46 +0000

    Adding new graphs interface.
    git-svn-id: https://svn.surfnet.nl/svn/ids/logserver/trunk@114 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

---
+++ webinterface/plotter.php
@@ -0,0 +1,192 @@
+<?php include("menu.php"); set_title("History graphs");
+
+####################################
+# SURFnet IDS                      #
+# Version 1.04.01                  #
+# 08-01-2007                       #
+# Kees Trippelvitz & Jan van Lith  #
+####################################
+
+#############################################
+# Changelog:
+# 1.04.01 Initial release
+#############################################
+?>
+
+<style type="text/css">@import url('./calendar/css/calendar.css');</style>
+<script type="text/javascript" src="./calendar/js/calendar.js"></script>
+<script type="text/javascript" src="./calendar/js/calendar-en.js"></script>
+<script type="text/javascript" src="./calendar/js/calendar-setup.js"></script>
+
+<?
+
+$s_org = intval($_SESSION['s_org']);
+$s_admin = intval($_SESSION['s_admin']);
+
+function getepoch($stamp) {
+  list($date, $time) = explode(" ", $stamp);
+  list($day, $mon, $year) = explode("-", $date);
+  list($hour, $min) = explode(":", $time);
+  // Date MUST BE valid
+  $day = intval($day);
+  $mon = intval($mon);
+  $year = intval($year);
+  if (($day > 0) && ($mon > 0) && ($year > 0)) {
+    if (checkdate($mon, $day, $year)) {
+      // Valid date, check time
+      $hour = intval($hour);
+      $min = intval($min);
+      if (!(($minute >= 0) && ($min < 60) && ($hour >= 0) && ($hour < 24))) {
+        // Invalid time, generate midnight (0:00)
+        $hour = $min = 0;
+      }
+      $epoch = mktime($hour, $min, 0, $mon, $day, $year);
+      return $epoch;
+    }
+  }
+}
+
+if ($_GET) {
+  $qs = $_SERVER['QUERY_STRING'];
+  echo "<img src='showplot.php?$qs'><br />\n";
+} else {
+  if ($s_admin == 1) {
+    $where = "";
+  } else {
+    $where = "organisations.id = $s_org";
+  }
+  $sql_getsensors = "SELECT sensors.id, sensors.keyname, sensors.vlanid, organisations.organisation FROM sensors, organisations ";
+  $sql_getsensors .= "WHERE organisations.id = sensors.organisation $where ORDER BY sensors.keyname";
+  $debuginfo[] = $sql_getsensors;
+  $result_getsensors = pg_query($sql_getsensors);
+
+  echo "<form method='get' action='$_SELF' name='plotform' id='plotform'>\n";
+    echo "<table class='datatable'>\n";
+      echo "<tr class='datatr'>\n";
+        echo "<td class='datatd' width='100'>From:</td>\n";
+        echo "<td class='datatd' width='300'>";
+          echo "<input type='text' name='strip_html_escape_tsstart' id='ts_start' value='' />\n";
+          echo "<input type='button' value='...' name='ts_start_trigger' id='ts_start_trigger' />\n";
+        echo "</td>\n";
+      echo "</tr>\n";
+      echo "<tr>\n";
+        echo "<td class='datatd'>To:</td>\n";
+        echo "<td class='datatd'>";
+          echo "<input type='text' name='strip_html_escape_tsend' id='ts_end' value='' />\n";
+          echo "<input type='button' value='...' name='ts_end_trigger' id='ts_end_trigger' />\n";
+        echo "</td>\n";
+      echo "</tr>\n";
+      # SENSORS
+      echo "<tr>\n";
+        echo "<td class='datatd'>Sensors:</td>\n";
+        echo "<td class='datatd'>\n";
+          echo "<select name='sensorid[]' style='background-color:white;' size='4' multiple='true'>\n";
+            echo printOption(0, "All sensors", $sensorid);
+            while ($sensor_data = pg_fetch_assoc($result_getsensors)) {
+              $sid = $sensor_data['id'];
+              $label = $sensor_data["keyname"];
+              $vlanid = $sensor_data["vlanid"];
+              $org = $sensor_data["organisation"];
+              if ($vlanid != 0 ) {
+                $label .=  "-" .$vlanid;
+              }
+              echo printOption($sid, $label, $sensorid);
+            }
+           echo "</select>\n";
+        echo "</td>\n";
+      echo "</tr>\n";
+      # SEVERITY
+      echo "<tr>\n";
+        echo "<td class='datatd'>Severity:</td>\n";
+        echo "<td class='datatd'>\n";
+          echo "<select name='severity[]' style='background-color:white;' size='5' multiple='true'>\n";
+            echo printOption(99, "All attacks", 99);
+            foreach ($severity_ar as $key => $val) {
+              echo printOption($key, $val, 99);
+            }
+           echo "</select>\n";
+        echo "</td>\n";
+      echo "</tr>\n";
+      # ATTACKS
+      $sql_getattacks = "SELECT * FROM stats_dialogue";
+      $debuginfo[] = $sql_getattacks;
+      $result_getattacks = pg_query($sql_getattacks);
+
+      echo "<tr>\n";
+        echo "<td class='datatd'>Attack:</td>\n";
+        echo "<td class='datatd'>\n";
+          echo "<select name='attack[]' style='background-color:white;' size='5' multiple='true'>\n";
+            echo printOption(99, "All attacks", 99);
+            while ($attack_data = pg_fetch_assoc($result_getattacks)) {
+              $id = $attack_data['id'];
+              $name = $attack_data['name'];
+              $name = str_replace("Dialogue", "", $name);
+              echo printOption($id, $name, 99); 
+            }
+           echo "</select>\n";
+        echo "</td>\n";
+      echo "</tr>\n";
+      echo "<tr>\n";
+        echo "<td class='datatd'>Interval</td>\n";
+        echo "<td class='datatd'>\n";
+          echo "<select name='int_interval'>\n";
+            echo printOption(0, "", 0);
+            echo printOption(3600, "Hour", 0);
+            echo printOption(86400, "Day", 0);
+            echo printOption(604800, "Week", 0);
+          echo "</select>\n";
+        echo "</td>\n";
+      echo "</tr>\n";
+      echo "<tr>\n";
+        echo "<td class='datatd'>Plot type</td>\n";
+        echo "<td class='datatd'>\n";
+          echo "<select name='int_type'>\n";
+            echo printOption(0, "", 0);
+            foreach ($v_plottertypes as $key => $val) {
+              echo printOption($key, $val, 0);
+            }
+          echo "</select>\n";
+        echo "</td>\n";
+      echo "</tr>\n";
+      echo "<tr>\n";
+        echo "<td colspan='2' align='right'><input type='submit' name='submit' value='Show' class='button' /></td>\n";
+      echo "</tr>\n";
+    echo "</table>\n";
+  echo "</form>\n";
+  debug_sql();
+?>
+<script>
+function catcalc(cal) {
+    var date = cal.date;
+    var time = date.getTime()
+    // use the _other_ field
+    var field = document.getElementById("ts_end");
+    time += Date.WEEK; // add one week
+    var date2 = new Date(time);
+    field.value = date2.print("%d-%m-%Y %H:%M");
+}
+
+Calendar.setup(
+  {
+      inputField  : "ts_start",
+      ifFormat    : "%d-%m-%Y %H:%M",
+      button      : "ts_start_trigger",
+      showsTime   : true,
+      singleClick : false,
+      onUpdate    : catcalc
+  }
+);
+Calendar.setup(
+  {
+      inputField  : "ts_end",
+      ifFormat    : "%d-%m-%Y %H:%M",
+      button      : "ts_end_trigger",
+      showsTime   : true,
+      singleClick : false
+  }
+);
+</script>
+<?php
+}
+?>
+<?php footer(); ?>
---
+++ webinterface/showplot.php
@@ -0,0 +1,382 @@
+<?php
+
+####################################
+# SURFnet IDS                      #
+# Version 1.04.01                  #
+# 08-01-2007                       #
+# Jan van Lith & Kees Trippelvitz  #
+####################################
+
+#############################################
+# Changelog:
+# 1.04.01 Initial release
+#############################################
+
+include '../include/config.inc.php';
+include '../include/connect.inc.php';
+include '../include/functions.inc.php';
+include '../include/variables.inc.php';
+
+#header("Content-type: image/png");
+#header("Cache-control: no-cache");
+#header("Pragma: no-cache");
+#header("Content-disposition: attachment; filename=plot.jpg");
+
+function getepoch($stamp) {
+  list($date, $time) = explode(" ", $stamp);
+  list($day, $mon, $year) = explode("-", $date);
+  list($hour, $min) = explode(":", $time);
+  // Date MUST BE valid
+  $day = intval($day);
+  $mon = intval($mon);
+  $year = intval($year);
+  if (($day > 0) && ($mon > 0) && ($year > 0)) {
+    if (checkdate($mon, $day, $year)) {
+      // Valid date, check time
+      $hour = intval($hour);
+      $min = intval($min);
+      if (!(($minute >= 0) && ($min < 60) && ($hour >= 0) && ($hour < 24))) {
+        // Invalid time, generate midnight (0:00)
+        $hour = $min = 0;
+      }
+      $epoch = mktime($hour, $min, 0, $mon, $day, $year);
+      return $epoch;
+    }
+  }
+}
+
+$allowed_get = array(
+                "int_interval",
+                "strip_html_escape_tsstart",
+                "strip_html_escape_tsend",
+		"int_type",
+		"sensorid",
+		"severity",
+		"attack"
+);
+$check = extractvars($_GET, $allowed_get);
+#debug_input();
+
+$allattacks = 0;
+$allsensors = 0;
+$allsev = 0;
+$groupbysensorid = "";
+$groupbyattacks = "";
+$selectattacks = "";
+$selectsensorid = "";
+$typecheck = 0;
+$where = "";
+
+########################
+# Severity
+########################
+if (@is_array($tainted['severity'])) {
+  $count = count($tainted['severity']);
+  if ($count == 1) {
+    $sev = intval($tainted['severity'][0]);
+    if ($sev == 99) {
+      $title .= "All attacks";
+      $allsev = 1;
+      $typecheck = 1;
+    } else {
+      $title .= $severity_ar[$sev];
+      $where .= " AND attacks.severity = $sev";
+    }
+  } else {
+    $where .= " AND attacks.severity IN (";
+    $check = 0;
+    foreach ($tainted['severity'] as $sev) {
+      $check++;
+      $sev = intval($sev);
+      if ($check != count($tainted['severity'])) {
+        $where .= $sev .", ";
+        $title .= $severity_ar[$sev] . ", ";
+      } else {
+        $where .= $sev;
+        $title .= $severity_ar[$sev];
+      }
+    }
+    $where .= ") ";
+  }
+} else {
+  $sev = intval($tainted['severity']);
+  if ($sev == 99) {
+    $title .= "All attacks";
+    $allsev = 1;
+    $typecheck = 1;
+  } else {
+    $title .= $severity_ar[$sev];
+    $where .= " AND attacks.severity = $sev";
+  }
+}
+
+########################
+# Attack Types
+########################
+if ($typecheck == 1) {
+  if (@is_array($tainted['attack'])) {
+    $count = count($tainted['attack']);
+    if ($count == 1) {
+      $attack = intval($tainted['attack'][0]);
+      if ($attack != 99) {
+        $groupbyattacks = "details.text, ";
+        $selectattacks = "details.text, ";
+
+        $sql_getattack = "SELECT name FROM stats_dialogue WHERE id = $attack";
+        $debuginfo[] = $sql_getattack;
+        $result_getattack = pg_query($sql_getattack);
+        $row_attack = pg_fetch_assoc($result_getattack);
+        $dianame = $row_attack['name'];
+
+        $attackname = str_replace("Dialogue", "", $dianame);
+        $title .= " ($attackname)";
+        $where .= " AND details.text = '$dianame'";
+      } else {
+        echo "E<br />\n";
+        $allattacks = 1;
+      }
+    } else {
+      $where .= " AND details.text IN (";
+      $check = 0;
+      $groupbyattacks = "details.text, ";
+      $selectattacks = "details.text, ";
+      $title .= " (";
+      foreach ($tainted['attack'] as $attack) {
+        $check++;
+        $attack = intval($attack);
+
+        $sql_getattack = "SELECT name FROM stats_dialogue WHERE id = $attack";
+        $debuginfo[] = $sql_getattack;
+        $result_getattack = pg_query($sql_getattack);
+        $row_attack = pg_fetch_assoc($result_getattack);
+        $dianame = $row_attack['name'];
+        $attackname = str_replace("Dialogue", "", $dianame);
+
+        if ($check != count($tainted['attack'])) {
+          $where .= "'" .$dianame. "', ";
+          $title .= $attackname .", ";
+        } else {
+          $where .= "'" .$dianame. "'";
+          $title .= $attackname;
+        }
+      }
+      $where .= ") ";
+      $title .= ") ";
+    }
+  } else {
+    $attack = intval($tainted['attack']);
+    if ($attack != 99) {
+      $groupbyattacks = "details.text, ";
+      $selectattacks = "details.text, ";
+
+      $sql_getattack = "SELECT name FROM stats_dialogue WHERE id = $attack";
+      $debuginfo[] = $sql_getattack;
+      $result_getattack = pg_query($sql_getattack);
+      $row_attack = pg_fetch_assoc($result_getattack);
+      $dianame = $row_attack['name'];
+      $attackname = str_replace("Dialogue", "", $dianame);
+
+      $title .= " ($attackname)";
+      $where .= " AND details.text = $dianame";
+    } else {
+      $allattacks = 1;
+    }
+  }
+  $where .= " AND attacks.severity = 1 ";
+}
+
+########################
+# Interval & Timestamps
+########################
+$interval = $clean['interval'];
+if ($interval == 3600) {
+  $title .= " per hour";
+} elseif ($interval == 86400) {
+  $title .= " per day";
+} elseif ($interval == 604800) {
+  $title .= " per week";
+}
+$tsstart = $clean['tsstart'];
+$tsend = $clean['tsend'];
+#$textstart = date("d-m-y H:m", $tsstart);
+#$textend = date("d-m-y H:m", $tsend);
+$textstart = $clean['tsstart'];
+$textend = $clean['tsend'];
+$tsstart = getepoch($tsstart);
+$tsend = getepoch($tsend);
+$tsperiod = $tsend - $tsstart;
+#echo "INTERVAL: $interval<br />\n";
+#echo "TSPERIOD: $tsperiod\n";
+$tssteps = intval($tsperiod / $interval);
+
+########################
+# Type
+########################
+$type = $clean['type'];
+$type = $v_plottertypes[$type];
+
+########################
+# Sensor ID
+########################
+if (@is_array($tainted["sensorid"])) {
+  $count = count($tainted['sensorid']);
+  if ($count == 1) {
+    $sid = intval($tainted['sensorid'][0]);
+    if ($sid == 0) {
+      $groupbysensorid = "";
+      $selectsensorid = "";
+      $title .= " for all sensors";
+      $allsensors = 1;
+    } else {
+      $groupbysensorid = "sensors.keyname, ";
+      $selectsensorid = "sensors.keyname, ";
+      $sql_gs = "SELECT keyname FROM sensors WHERE id = $sid";
+      $result_gs = pg_query($pgconn, $sql_gs);
+      $row_gs = pg_fetch_assoc($result_gs);
+      $keyname = $row_gs['keyname'];
+      $title .= " for $keyname";
+      $where .= " AND sensors.id = $sid";
+    }
+  } else {
+    $groupbysensorid = "sensors.keyname, ";
+    $selectsensorid = "sensors.keyname, ";
+    $title .= " for sensors: ";
+    $where .= " AND sensors.id IN (";
+    $check = 0;
+    foreach ($tainted['sensorid'] as $sid) {
+      $check++;
+      $sid = intval($sid);
+      $sql_gs = "SELECT keyname FROM sensors WHERE id = $sid";
+      $result_gs = pg_query($pgconn, $sql_gs);
+      $row_gs = pg_fetch_assoc($result_gs);
+      $keyname = $row_gs['keyname'];
+      if ($check != count($tainted['sensorid'])) {
+        $where .= $sid .", ";
+        $title .= "$keyname, ";
+      } else {
+        $where .= $sid;
+        $title .= $keyname;
+      }
+    }
+    $where .= ") ";
+  }
+} else {
+  $sid = intval($tainted['sensorid']);
+  if ($sid == 0) {
+    $groupbysensorid = "";
+    $selectsensorid = "";
+    $title .= " for all sensors";
+    $allsensors = 1;
+  } else {
+    $groupbysensorid = "sensors.keyname, ";
+    $selectsensorid = "sensors.keyname, ";
+    $sql_gs = "SELECT keyname FROM sensors WHERE id = $sid";
+    $result_gs = pg_query($pgconn, $sql_gs);
+    $row_gs = pg_fetch_assoc($result_gs);
+    $keyname = $row_gs['keyname'];
+    $title .= " for $keyname";
+    $where .= " AND sensors.id = $sid";
+  }
+}
+
+$title .= "\n From $textstart to $textend";
+
+##############
+# PHPlot stuff
+##############
+require_once '/usr/share/phplot/phplot.php';  // here we include the PHPlot code 
+$plot =& new PHPlot(1000,600);    // here we define the variable
+$plot->SetTitle($title);
+$plot->SetXTitle('Time');
+$plot->SetYTitle('Attacks');
+$plot->SetPlotType($type);
+
+$data = array();
+$i = 0;
+
+$check_ar = array();
+$maxcount = 0;
+
+while ($i != $tssteps) {
+  $a = $i;
+  $i++;
+  $point = array();
+  $sql = "SELECT DISTINCT $selectattacks $selectsensorid attacks.severity, COUNT(attacks.severity) as total FROM attacks, sensors, details ";
+  $sql .= "WHERE details.attackid = attacks.id AND timestamp >= $tsstart + ($interval * $a) AND timestamp <= $tsstart + ($interval * $i) ";
+  $sql .= "AND sensors.id = attacks.sensorid $where ";
+  $sql .= "GROUP BY $groupbysensorid $groupbyattacks attacks.severity ORDER BY severity";
+#  printer($sql);
+  $debuginfo[] = $sql;
+
+  $result = pg_query($pgconn, $sql);
+  $numrows = pg_num_rows($result);
+  if ($numrows != 0) {
+    $date = $tsstart + ($interval * $a);
+    if ($interval == 3600) {
+      $datestring = date("d-m-y", $date) . "\n " . date("H", $date) . ":00";
+    } elseif ($interval == 86400) {
+      $datestring = date("d-m-y", $date);
+    } elseif ($interval == 604800) {
+      $datestring = "Week\n" .date("W", $date);
+    }
+    $point[] = $datestring;
+#    printer($row);
+    while($row = pg_fetch_assoc($result)) {
+      $count = $row['total'];
+      $sev = $row['severity'];
+      if ($typecheck == 1) {
+        $type = $row['text'];
+      }
+      $legend = "";
+      if ($maxcount < $count) {
+        $maxcount = $count;
+      }
+      if ($allsensors == 0) {
+        $keyname = $row['keyname'];
+        $legend .= $keyname;
+      } else {
+        $legend .= "All sensors";
+      }
+      if ($allattacks == 0 && $typecheck == 1) {
+        $attack = str_replace("Dialogue", "", $row['text']);
+        $legend .= " - ". $attack;
+      }
+      $legend .= " - " .$severity_ar[$sev];
+      if (!in_array($legend, $check_ar)) {
+        $check_ar[] = $legend;
+        $plot->SetLegend($legend);
+      }
+      $point[$legend] = $count;
+    }
+    $data[] = $point;
+  }
+}
+
+#printer($data);
+
+if (!empty($data)) {
+#  printer($maxcount);
+  $pertick = intval($maxcount / 20);
+  if ($pertick > 50) {
+    $ytick = $pertick - ($pertick % 50);
+  } elseif ($pertick > 10) {
+    $ytick = $pertick - ($pertick % 10);
+  } elseif ($pertick > 5) {
+    $ytick = 10;
+  } elseif ($pertick != 0) {
+    $ytick = 5;
+  } else {
+    $ytick = 1;
+  }
+
+#  printer($pertick);
+#  printer($ytick);
+
+#  debug_sql();
+  $plot->SetYTickIncrement($ytick);
+  $plot->SetDataValues($data);
+  #$plot->SetXTickLabelPos('none');
+  #$plot->SetXTickPos('none');
+  $plot->DrawGraph();
+}
+?>
