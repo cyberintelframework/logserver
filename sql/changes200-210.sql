f4376734f55b136e76fd65bbc680e43dc9ae4f4b
Kees Trippelvitz <github@nabber.nl>
2008-05-13 15:32:48 +0000

    Added changes file for 2.10.
    git-svn-id: https://svn.surfnet.nl/svn/ids/2.10/logserver/trunk@768 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

---
+++ sql/changes200-210.sql
@@ -0,0 +1,35 @@
+-- SURFids 2.10.00
+-- Database conversion 2.00 -> 2.10
+-- Changeset 002
+-- 13-05-2008
+--
+
+--
+-- Changelog
+-- 002 Added LOGIN modifications
+-- 001 Initial release
+--
+
+--
+-- ARP_EXCL
+--
+
+CREATE TABLE arp_excl (
+    id serial NOT NULL,
+    mac macaddr NOT NULL
+);
+
+ALTER TABLE ONLY arp_excl
+    ADD CONSTRAINT arp_excl_mac_key UNIQUE (mac);
+
+ALTER TABLE ONLY arp_excl
+    ADD CONSTRAINT arp_excl_pkey PRIMARY KEY (id);
+
+GRANT INSERT,SELECT,UPDATE,DELETE ON TABLE arp_excl TO idslog;
+
+--
+-- LOGIN
+--
+ALTER TABLE attacks ADD COLUMN d_plotter integer DEFAULT 0 NOT NULL;
+ALTER TABLE attacks ADD COLUMN d_plottype integer DEFAULT 1 NOT NULL;
+ALTER TABLE attacks ADD COLUMN d_utc integer DEFAULT 0 NOT NULL;
