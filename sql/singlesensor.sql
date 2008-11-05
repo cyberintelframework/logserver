427ec13180899d73fd0ebb1b011f1735303e3f12
Kees Trippelvitz <github@nabber.nl>
2008-11-05 10:24:32 +0000

    New structure
    git-svn-id: https://svn.surfnet.nl/svn/ids/logserver/trunk@863 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- sql/singlesensor.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ONLY organisations ADD CONSTRAINT unique_organisations UNIQUE (organisation);
-INSERT INTO organisations (id, organisation) VALUES (nextval('organisations_id_seq'::regclass), 'NEPENTHES');
-UPDATE sensors SET organisation = (SELECT id FROM organisations WHERE organisation = 'NEPENTHES') WHERE organisation = 0;
