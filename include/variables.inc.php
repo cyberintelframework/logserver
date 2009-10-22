870be6a6435d31c627909bec1f5511d382f73a2a
Kees Trippelvitz <github@nabber.nl>
2009-10-22 13:50:34 +0000

    Removed v_attacks_ar. Adde Cymru to whois options.
    git-svn-id: https://svn.surfnet.nl/svn/ids/logserver/trunk@1368 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- include/variables.inc.php
+++ include/variables.inc.php
@@ -15,117 +15,6 @@
 # 001 Added selview array
 #############################################
 
-# The array with dialogue name, exploit name and additional info. Desc is not used at the moment.
-$v_attacks_ar = array(
-		'SMBDialogue'		=> array(
-						'Attack'	=> 'ASN1',
-						'Desc'		=> 'ASN1 exploit',
-						'URL'		=> 'http://www.microsoft.com/technet/security/bulletin/ms04-007.mspx'),
-		'BagleDialogue'		=> array(
-						'Attack'	=> 'Bagle',
-						'Desc'		=> 'Bagle worm',
-						'URL'		=> 'http://www.f-secure.com/v-descs/bagle.shtml'),
-		'DWDialogue'		=> array(
-						'Attack'	=> 'Dameware',
-						'Desc'		=> 'Dameware vulnerability',
-						'URL'		=> ''),
-		'DCOMDialogue'		=> array(
-						'Attack'	=> 'DCOM',
-						'Desc'		=> 'DCOM attack',
-						'URL'		=> 'http://nepenthes.sourceforge.net/documentation:modules:vulnerability:vuln_dcom'),
-		'FTPdDialogue'		=> array(
-						'Attack'	=> 'FTPD',
-						'Desc'		=> 'FTPD exploit',
-						'URL'		=> ''),
-		'IISDialogue'		=> array(
-						'Attack'	=> 'IIS',
-						'Desc'		=> 'IIS exploit',
-						'URL'		=> 'http://nepenthes.sourceforge.net/documentation:modules:vulnerability:vuln_iis'),
-		'Kuang2Dialogue'	=> array(
-						'Attack'	=> 'Kuang2',
-						'Desc'		=> 'Kuang2 exploit',
-						'URL'		=> 'http://www3.ca.com/securityadvisor/pest/search.aspx?mode=scan&allwords=true&pst=KUANG'),
-		'LSASSDialogue'		=> array(
-						'Attack'	=> 'LSASS',
-						'Desc'		=> 'LSASS vulnerability',
-						'URL'		=> 'http://www.microsoft.com/technet/security/bulletin/MS04-011.mspx'),
-		'MS06070Dialogue'	=> array(
-						'Attack'	=> 'Wkssvc',
-						'Desc'		=> 'MS06070 vulnerability',
-						'URL'		=> 'http://www.microsoft.com/technet/security/Bulletin/MS06-070.mspx'),
-		'MS08067Dialogue'	=> array(
-						'Attack'	=> 'MS08-067',
-						'Desc'		=> 'MS08-67 vulnerability',
-						'URL'		=> 'http://www.microsoft.com/technet/security/Bulletin/MS08-067.mspx'),
-		'MSDTCDialogue'		=> array(
-						'Attack'	=> 'MSDTC/Dasher',
-						'Desc'		=> 'Dasher worm',
-						'URL'		=> 'http://www.microsoft.com/technet/security/Bulletin/MS05-051.mspx'),
-		'MSMQDialogue'		=> array(
-						'Attack'	=> 'MSMQ',
-						'Desc'		=> 'MSMQ exploit',
-						'URL'		=> 'http://www.microsoft.com/technet/security/bulletin/MS05-017.mspx'),
-		'MSSQLDialogue'		=> array(
-						'Attack'	=> 'MSSQL',
-						'Desc'		=> 'MS02-061 exploit',
-						'URL'		=> 'http://www.microsoft.com/technet/security/bulletin/MS02-039.mspx'),
-		'MydoomDialogue'	=> array(
-						'Attack'	=> 'MyDoom',
-						'Desc'		=> 'MyDoom worm',
-						'URL2'		=> 'http://vil.nai.com/vil/content/v_100983.htm'),
-		'NETDDEDialogue'	=> array(
-						'Attack'	=> 'NetDDE',
-						'Desc'		=> 'NetDDE attack',
-						'URL'		=> 'http://www.microsoft.com/technet/security/Bulletin/MS04-031.mspx'),
-		'OPTIXBindDialogue'	=> array(
-						'Attack'	=> 'OPTIX Bind',
-						'Desc'		=> 'OPTIX Bind dialogue used for timeouts',
-						'URL2'		=> 'http://www3.ca.com/securityadvisor/pest/search.aspx?mode=tmc&pst=optix&allwords=false&nameonly=false&type=6'),
-		'OPTIXShellDialogue'	=> array(
-						'Attack'	=> 'OPTIX Shell',
-						'Desc'		=> 'OPTIX Shell Dialogue',
-						'URL'		=> 'http://www3.ca.com/securityadvisor/pest/search.aspx?mode=tmc&pst=optix&allwords=false&nameonly=false&type=6'),
-		'OPTIXDownloadDialogue'	=> array(
-						'Attack'	=> 'OPTIX Download',
-						'Desc'		=> 'OPTIX Download Dialogue',
-						'URL'		=> 'http://www3.ca.com/securityadvisor/pest/search.aspx?mode=tmc&pst=optix&allwords=false&nameonly=false&type=6'),
-		'PNPDialogue'		=> array(
-						'Attack'	=> 'PNP',
-						'Desc'		=> 'PNP Vulnerability',
-						'URL'		=> 'http://www.microsoft.com/technet/security/Bulletin/MS05-039.mspx'),
-		'RealVNCDialogue'	=> array(
-						'Attack'	=> 'RealVNC',
-						'Desc'		=> 'RealVNC Vulnerability',
-						'URL'		=> ''),
-		'SasserFTPDDialogue'	=> array(
-						'Attack'	=> 'Sasser',
-						'Desc'		=> 'Sasser exploit',
-						'URL2'		=> 'http://www.microsoft.com/security/incident/sasser.mspx'),
-		'SAVDialogue'		=> array(
-						'Attack'	=> 'Symantec AV',
-						'Desc'		=> 'Symantec AntiVirus software bug',
-						'URL'		=> 'http://www.symantec.com/avcenter/security/Content/2005.10.04.html'),
-		'SSHDialogue'		=> array(
-						'Attack'	=> 'SSH vulnerability',
-						'Desc'		=> 'SSH vulnerability simulation'),
-		'SUB7Dialogue'		=> array(
-						'Attack'	=> 'Sub7',
-						'Desc'		=> 'Sub7 worm',
-						'URL'		=> 'http://www3.ca.com/securityadvisor/pest/search.aspx?mode=tmc&pst=sub7&allwords=false&nameonly=false&type=6'),
-		'UPNPDialogue'		=> array(
-						'Attack'	=> 'UPNP',
-						'Desc'		=> 'UPNP vulnerability',
-						'URL'		=> 'http://www.microsoft.com/technet/security/bulletin/MS01-059.mspx'),
-		'VERITASDialogue'	=> array(
-						'Attack'	=> 'Veritas',
-						'Desc'		=> 'Veritas vulnerability',
-						'URL'		=> ''),
-		'WINSDialogue'		=> array(
-						'Attack'	=> 'WINS',
-						'Desc'		=> 'WINS vulnerability',
-						'URL'		=> 'http://nepenthes.sourceforge.net/documentation:modules:vulnerability:vuln_wins')
-);
-
 # Array with the severity of the attack.
 $v_severity_ar = array(
         '0'	=> 'Possible malicious attack',
@@ -476,7 +365,8 @@ $v_whois_servers = array(
 	'lacnic' => "LACNIC",
 	'afrinic' => "AFRINIC",
 	'krnic' => "KRNIC",
-	'jpnic' => "JPNIC"
+	'jpnic' => "JPNIC",
+    'cymru' => "CYMRU"
 );
 
 # Array with the different destination search option
