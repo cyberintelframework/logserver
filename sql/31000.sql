15017baa2e0f66d4aa5bc558a2274df6dacfead7
Kees Trippelvitz <github@nabber.nl>
2010-03-15 14:46:00 +0000

    Updated version and removed version column in sniff_protos.
    git-svn-id: https://svn.surfnet.nl/svn/ids/logserver/trunk@1576 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- sql/31000.sql
+++ sql/31000.sql
@@ -1,13 +1,14 @@
 -- SURFids 3.10
 -- Database conversion 3.00 -> 3.10
--- Changeset 002
--- 17-02-2010
+-- Changeset 003
+-- 15-03-2010
 --
 
 --
 -- Changelog
--- 001 Initial release
+-- 003 Updated version
 -- 002 Added mod_malhosts.php to indexmods
+-- 001 Initial release
 --
 
 --
@@ -16,7 +17,6 @@
 
 ALTER TABLE sniff_protos DROP COLUMN protocol;
 ALTER TABLE sniff_protos ADD COLUMN subtype integer;
-ALTER TABLE sniff_protos ADD COLUMN version integer;
 
 --
 -- REPORT_CONTENT
@@ -29,3 +29,8 @@ ALTER TABLE report_content ADD COLUMN orgid integer DEFAULT 0 NOT NULL;
 -- INDEXMODS
 --
 INSERT INTO indexmods ('phppage') values ('mod_malhosts.php');
+
+--
+-- VERSION
+--
+UPDATE version SET version = '31000';
