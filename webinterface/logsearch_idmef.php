7207c32b02c50c006d5b83e6e039e6a29cdb21be
Kees Trippelvitz <github@nabber.nl>
2010-12-27 15:52:00 +0000

    Fixed idmef page to use sql builder as well.
    git-svn-id: https://svn.surfnet.nl/svn/ids/logserver/trunk@1747 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- webinterface/logsearch_idmef.php
+++ webinterface/logsearch_idmef.php
@@ -66,6 +66,11 @@ $allowed_get = array(
     	"int_binid",
         "int_sourcechoice",
         "int_destchoice",
+        "int_interval",
+        "int_gid",
+        "int_macfilter",
+        "int_ipfilter",
+        "int_allexploits",
         "strip_html_escape_sshversion",
         "strip_html_escape_sshuser",
         "strip_html_escape_sshpass",
@@ -85,21 +90,13 @@ $search = $clean['search'];
 reset_sql();
 
 $q_org = $_SESSION['q_org'];
-
-### Setting values from searchform
-if (@is_array($tainted["sensorid"])) {
-  if ($tainted['sensorid'][0] != 0) {
-    $sensorid = -1;
-    $ar_sensorid = array();
-    foreach ($tainted["sensorid"] as $sid) {
-      $ar_sensorid[] = intval($sid);
-    }
-  } else {
-    $sensorid = 0;
-  }
-} else $sensorid = intval($tainted['sensorid']);
 $to = $_SESSION['s_to'];
 $from = $_SESSION['s_from'];
+
+####################
+# INCLUDE SQL BUILDER
+####################
+include_once 'search_sqlbuilder.php';
 
 ####################
 # Default censor value
@@ -119,248 +116,12 @@ if (isset($clean['dcensor'])) {
     $d_censor = $row['d_censor'];
 }
 
-####################
-# Severity
-####################
-if (isset($clean['sev'])) {
-  $f_sev = $clean['sev'];
-  if (!array_key_exists($f_sev, $v_severity_ar)) {
-    unset($f_sev);
-  }
-}
-
-####################
-# Severity Type
-####################
-if (isset($clean['sevtype'])) {
-  $f_sevtype = $clean['sevtype'];
-  if (!array_key_exists($f_sevtype, $v_severity_atype_ar)) {
-    unset($f_sevtype);
-  }
-}
-
-####################
-# Binary name
-####################
-$bin_pattern = '/^[a-zA-Z0-9%]{1,33}$/';
-if (preg_match($bin_pattern, $clean['binname'])) {
-  $f_binname = $clean['binname'];
-}
-
-####################
-# Binary ID
-####################
-$f_binid = $clean['binid'];
-
-####################
-# Attack type
-####################
-$f_attack = $clean['attack'];
-
-####################
-# Virus type
-####################
-$f_virus_txt = $clean['virustxt'];
-
-####################
-# Filename
-####################
-$f_filename = $clean['filename'];
-
-####################
-# Report type
-####################
-$f_reptype = $rapport;
-
-####################
-# Sensor ID's
-####################
-if ($sensorid > 0) {
-  add_to_sql("sensors", "table");
-  add_to_sql("sensors.id = '$sensorid'", "where");
-} elseif ($sensorid == -1) {
-  # multiple sensors
-  add_to_sql("sensors", "table");
-  $count = count($ar_sensorid);
-  $tmp_where = "sensors.id IN (";
-  for ($i = 0; $i < $count; $i++) {
-    if ($i != ($count - 1)) {
-      $tmp_where .= "$ar_sensorid[$i], ";
-    } else {
-      $tmp_where .= "$ar_sensorid[$i]";
-    }
-  }
-  $tmp_where .= ")";
-  add_to_sql($tmp_where, "where");
-}
-
-####################
-# Source IP address
-####################
-if (isset($clean['sourcemac'])) {
-  $source_mac = $clean['sourcemac'];
-  add_to_sql("attacks", "table");
-  add_to_sql("attacks.src_mac = '$source_mac'", "where");
-}
-if (isset($clean['source'])) {
-  $source_ip = $clean['source'];
-  add_to_sql("attacks", "table");
-  add_to_sql("attacks.source <<= '$source_ip'", "where");
-}
-if (isset($clean['ownsource'])) {
-  $ownsource = $clean['ownsource'];
-  add_to_sql("attacks", "table");
-  add_to_sql("attacks.source <<= '$ownsource'", "where");
-}
-if (isset($clean['sport'])) {
-  $sport = $clean['sport'];
-  if ($sport != 0) {
-    add_to_sql("attacks", "table");
-    add_to_sql("attacks.sport = '$sport'", "where");
-  }
-}
-
-####################
-# Destination IP address
-####################
-if (isset($clean['destmac'])) {
-  $dest_mac = $clean['destmac'];
-  add_to_sql("attacks", "table");
-  add_to_sql("attacks.dst_mac = '$dest_mac'", "where");
-}
-if (isset($clean['dest'])) {
-  $destination_ip = $clean['dest'];
-  add_to_sql("attacks", "table");
-  add_to_sql("attacks.dest <<= '$destination_ip'", "where");
-}
-if (isset($clean['dport'])) {
-  $dport = $clean['dport'];
-  if ($dport != 0) {
-    add_to_sql("attacks", "table");
-    add_to_sql("attacks.dport = '$dport'", "where");
-  }
-}
-
-####################
-# Start timestamp
-####################
-add_to_sql("attacks", "table");
-add_to_sql("attacks.timestamp >= '$from'", "where");
-
-####################
-# End timestamp
-####################
-add_to_sql("attacks", "table");
-add_to_sql("attacks.timestamp <= '$to'", "where");
-
-####################
-# Severity
-####################
-if (isset($f_sev)) {
-  add_to_sql("attacks", "table");
-  add_to_sql("attacks.severity = '$f_sev'", "where");
-}
-
-####################
-# Severity type
-####################
-if (isset($f_sevtype)) {
-  add_to_sql("attacks", "table");
-  add_to_sql("attacks.atype = '$f_sevtype'", "where");
-}
-
-####################
-# Type of attack
-####################
-if ($f_attack > 0) {
-  add_to_sql("details", "table");
-  add_to_sql("stats_dialogue", "table");
-  add_to_sql("attacks.id = details.attackid", "where");
-  add_to_sql("details.type = 1", "where");
-  add_to_sql("details.text = stats_dialogue.name", "where");
-  add_to_sql("stats_dialogue.id = '$f_attack'", "where");
-}
-
-####################
-# Type of virus
-####################
-if (!empty($f_virus_txt)) {
-  add_to_sql("binaries", "table");
-  add_to_sql("details", "table");
-  add_to_sql("stats_virus", "table");
-  add_to_sql("uniq_binaries", "table");
-  add_to_sql("attacks.id = details.attackid", "where");
-  add_to_sql("details.type = 8", "where");
-  add_to_sql("details.text = uniq_binaries.name", "where");
-  add_to_sql("uniq_binaries.id = binaries.bin", "where");
-  add_to_sql("binaries.info = stats_virus.id", "where");
-  add_to_sql("stats_virus.name LIKE '$f_virus_txt'", "where");
-  add_to_sql("details.text", "select");
-}
-
-####################
-# Filename
-####################
-if (!empty($f_filename)) {
-  add_to_sql("details", "table");
-  add_to_sql("attacks.id = details.attackid", "where");
-  add_to_sql("details.type = 4", "where");
-  add_to_sql("details.text LIKE '%$f_filename%'", "where");
-  add_to_sql("details.text", "select");
-}
-
-####################
-# Binary Name
-####################
-if (!empty($f_binname)) {
-  add_to_sql("details", "table");
-  add_to_sql("details.type = 8", "where");
-  add_to_sql("attacks.id = details.attackid", "where");
-  add_to_sql("details.text LIKE '$f_binname'", "where");
-}
-
-####################
-# Binary ID
-####################
-if (!empty($f_binid)) {
-  add_to_sql("details", "table");
-  add_to_sql("uniq_binaries", "table");
-  add_to_sql("attacks.id = details.attackid", "where");
-  add_to_sql("details.type = 8", "where");
-  add_to_sql("details.text = uniq_binaries.name", "where");
-  add_to_sql("uniq_binaries.id = $f_binid", "where");
-}
-
-####################
-# Ranges
-####################
-if ($f_sourcechoice == 3 && $ownsource == "") {
-  add_to_sql(gen_org_sql(1), "where");
-} else {
-  add_to_sql(gen_org_sql(), "where");
-}
-
-add_to_sql("sensors.keyname", "select");
-add_to_sql("sensors.vlanid", "select");
-add_to_sql("sensors.label", "select");
-add_to_sql("attacks.*", "select");
-add_to_sql("sensors", "table");
-add_to_sql("attacks", "table");
-add_to_sql("sensors.id = attacks.sensorid", "where");
-
-# IP Exclusion stuff
-add_to_sql("NOT attacks.source IN (SELECT exclusion FROM org_excl WHERE orgid = $q_org)", "where");
-# MAC Exclusion stuff
-add_to_sql("(attacks.src_mac IS NULL OR NOT attacks.src_mac IN (SELECT mac FROM arp_excl))", "where");
-
-prepare_sql();
-
 ### Prepare final SQL query
 $sql = "SELECT $sql_select ";
 $sql .= " FROM $sql_from ";
 $sql .= " $sql_where ";
 $sql .= " $sql_group ";
-    
+
 $result = pg_query($sql);
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
@@ -385,29 +146,7 @@ while ($row = pg_fetch_assoc($result)) {
   $dport = intval($row['dport']);
   $sev = intval($row['severity']);
   $sevtype = intval($row['atype']);
-  $sql_details = "SELECT id, text, type FROM details WHERE attackid = " . $id;
-  $result_details = pg_query($pgconn, $sql_details);
-  $numrows_details = pg_num_rows($result_details);
- 
-  $sev_text = "$v_severity_ar[$sev]-$v_severity_atype_ar[$sevtype]";
 
-  if ($numrows_details != 0) {
-    if ($sev == 1) {
-      $dia_ar = array('attackid' => $id, 'type' => 1);
-      $dia_result_ar = pg_select($pgconn, 'details', $dia_ar);
-      $text = $dia_result_ar[0]['text'];
-      $attack = str_replace("Dialogue", "", $text);
-    } elseif ($sev == 16) {
-      $dia_ar = array('attackid' => $id);
-      $dia_result_ar = pg_select($pgconn, 'details', $dia_ar);
-      $text = $dia_result_ar[0]['text'];
-      $malware = basename($text);
-    } elseif ( ( $sev == 1 || $sev == 0 ) && $sevtype == 2) {
-      $dia_ar = array('attackid' => $id, 'type' => 40);
-      $dia_result_ar = pg_select($pgconn, 'details', $dia_ar);
-      $module = $dia_result_ar[0]['text'];
-    }
-  }
   echo "<idmef:Alert messageid=\"$id\">\n";
   echo "  <idmef:Analyzer analyzerid=\"$keyname\">\n";
   echo "  </idmef:Analyzer>\n";
@@ -440,12 +179,81 @@ while ($row = pg_fetch_assoc($result)) {
   echo "    </idmef:Service>\n";
   echo "  </idmef:Target>\n";
 
+  #########################
+  # DETAILS
+  #########################
+
+  if ($sevtype == 7) {
+    # SSH version
+    $sql_version = "SELECT uniq_sshversion.version FROM ssh_version, uniq_sshversion WHERE attackid = $id";
+    $result_version = pg_query($pgconn, $sql_version);
+    $row_v = pg_fetch_assoc($result_version);
+    $sshversion = $row_v['version'];
+
+    echo "  <idmef:AdditionalData type=\"string\" meaning=\"ssh-version\">\n";
+    echo "    <idmef:string>$sshversion</idmef:string>\n";
+    echo "  </idmef:AdditionalData>\n";
+
+    # SSH login
+    $sql_login = "SELECT sshuser, sshpass, type FROM ssh_logins WHERE attackid = $id";
+    $result_login = pg_query($pgconn, $sql_login);
+    $row_l = pg_fetch_assoc($result_login);
+    $sshuser = $row_l['sshuser'];
+    $sshpass = $row_l['sshpass'];
+    $logintype = $row_l['type'];
+
+    echo "  <idmef:AdditionalData type=\"string\" meaning=\"ssh-user\">\n";
+    echo "    <idmef:string>$sshuser</idmef:string>\n";
+    echo "  </idmef:AdditionalData>\n";
+    echo "  <idmef:AdditionalData type=\"string\" meaning=\"ssh-pass\">\n";
+    echo "    <idmef:string>$sshpass</idmef:string>\n";
+    echo "  </idmef:AdditionalData>\n";
+    echo "  <idmef:AdditionalData type=\"string\" meaning=\"login-type\">\n";
+    echo "    <idmef:string>$logintype</idmef:string>\n";
+    echo "  </idmef:AdditionalData>\n";
+
+    # SSH commands
+    $sql_command = "SELECT command FROM ssh_command WHERE attackid = $id";
+    $result_command = pg_query($pgconn, $sql_command);
+    while ($row_c = pg_fetch_assoc($result_command)) {
+      $sshcommand = $row_c['command'];
+
+      echo "  <idmef:AdditionalData type=\"string\" meaning=\"ssh-command\">\n";
+      echo "    <idmef:string>$sshcommand</idmef:string>\n";
+      echo "  </idmef:AdditionalData>\n";
+    }
+  } else {
+    $sql_details = "SELECT id, text, type FROM details WHERE attackid = " . $id;
+    $result_details = pg_query($pgconn, $sql_details);
+    $numrows_details = pg_num_rows($result_details);
+ 
+    $sev_text = "$v_severity_ar[$sev]-$v_severity_atype_ar[$sevtype]";
+
+    if ($numrows_details != 0) {
+      if ($sev == 1) {
+        $dia_ar = array('attackid' => $id, 'type' => 1);
+        $dia_result_ar = pg_select($pgconn, 'details', $dia_ar);
+        $text = $dia_result_ar[0]['text'];
+        $attack = str_replace("Dialogue", "", $text);
+      } elseif ($sev == 16) {
+        $dia_ar = array('attackid' => $id);
+        $dia_result_ar = pg_select($pgconn, 'details', $dia_ar);
+        $text = $dia_result_ar[0]['text'];
+        $malware = basename($text);
+      } elseif ( ( $sev == 1 || $sev == 0 ) && $sevtype == 2) {
+        $dia_ar = array('attackid' => $id, 'type' => 40);
+        $dia_result_ar = pg_select($pgconn, 'details', $dia_ar);
+        $module = $dia_result_ar[0]['text'];
+      }
+    }
+  }
+
   if ($sev == 1 && $attack != "") {
-    echo "  <idmef:AdditionalData type=\"string\" meaning=\"" .$l['ls_at']. "\">\n";
+    echo "  <idmef:AdditionalData type=\"string\" meaning=\"attack-type\">\n";
     echo "    <idmef:string>$attack</idmef:string>\n";
     echo "  </idmef:AdditionalData>\n";
   } elseif ($sev == 16 && $malware != "") {
-    echo "  <idmef:AdditionalData type=\"string\" meaning=\"" .$l['ls_fo']. "\">\n";
+    echo "  <idmef:AdditionalData type=\"string\" meaning=\"file-offered\">\n";
     echo "    <idmef:string>$malware</idmef:string>\n";
     echo "  </idmef:AdditionalData>\n";
   } elseif ( ( $sev == 1 || $sev == 0 ) && $sevtype == 2) {
