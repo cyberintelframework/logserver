4f263c8acd1c26b6e6e4d6fd39f099d2417696cc
Kees Trippelvitz <github@nabber.nl>
2011-05-19 09:56:30 +0000

    added non-used code for future upgrade of the country search
    git-svn-id: https://svn.surfnet.nl/svn/ids/logserver/trunk@1796 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- logtools/updateGeoIP.pl
+++ logtools/updateGeoIP.pl
@@ -34,15 +34,17 @@ if ($? != 0) {
 `gunzip /tmp/GeoLiteCity.dat.gz`;
 `mv /tmp/GeoLiteCity.dat $c_surfidsdir/include/`;
 
+exit;
+
 ##################
 # UPDATE CSV FILE
 ##################
 `wget -q -O /tmp/GeoIPCountryCSV.zip $csvfile`;
 
-if ($? != 0) {
-    print "Failed to fetch new GeoIP CSV file\n";
-    exit;
-}
+#if ($? != 0) {
+#    print "Failed to fetch new GeoIP CSV file\n";
+#    exit;
+#}
 
 # Unzip
 chdir("/tmp/");
@@ -50,7 +52,7 @@ chdir("/tmp/");
 
 # Truncate geo tables
 `sudo -u postgres psql -d $c_pgsql_dbname -c "TRUNCATE geolocations"`;
-`sudo -u postgres psql -d $c_pgsql_dbname -c "TRUNCATE geoblocks"`;
+`sudo -u postgres psql -d $c_pgsql_dbname -c "TRUNCATE geonewblocks"`;
 
 open(GEOIP, "< GeoIPCountryWhois.csv");
 while (<GEOIP>) {
@@ -77,20 +79,33 @@ while (<GEOIP>) {
         $cid = $i;
     }
 
+    print "$ipstart - $ipend\n";
+    # Get ip range
+    $temp = `ipcalc $ipstart - $ipend | grep -v deaggregate`;
+    @tempar = split(/\n/, $temp);
+    foreach $range (@tempar) {
+        chomp($range);
+        print "\t$range\n";
+        $sql = "INSERT INTO geonewblocks (locid, iprange) VALUES ($cid, '$range')";
+        $sth = $dbh->prepare($sql);
+        $er = $sth->execute();
+    }
+    
+
     # Check block
-    $sql = "INSERT INTO geoblocks (locid, ipstart, ipend) VALUES ($cid, '$ipstart', '$ipend')";
-    $sth = $dbh->prepare($sql);
-    $er = $sth->execute();
+    #$sql = "INSERT INTO geoblocks (locid, ipstart, ipend) VALUES ($cid, '$ipstart', '$ipend')";
+    #$sth = $dbh->prepare($sql);
+    #$er = $sth->execute();
 }
 close(GEOIP);
 
 # Cleanup
 if (-e "/tmp/GeoIPCountryCSV.zip") {
-    `rm /tmp/GeoIPCountryCSV.zip`;
+#    `rm /tmp/GeoIPCountryCSV.zip`;
 }
 if (-e "/tmp/GeoLiteCity.dat.gz") {
-    `rm /tmp/GeoLiteCity.dat.gz`;
+#    `rm /tmp/GeoLiteCity.dat.gz`;
 }
 if (-e "/tmp/GeoIPCountryWhois.csv") {
-    `rm /tmp/GeoIPCountryWhois.csv`;
+#    `rm /tmp/GeoIPCountryWhois.csv`;
 }
